a870ed3b0ede35c1685d438c4e4d90968a0315d4
bblay <byronblay@googlemail.com>
2012-12-19 09:59:31 +0000

    temp

--- install/temp.sh
+++ install/temp.sh
@@ -4,26 +4,21 @@
 # cartopy #
 ###########
 
-wget http://python-distribute.org/distribute_setup.py
-sudo python distribute_setup.py
-sudo rm distribute*.gz
-rm distribute_setup.py
+#wget http://python-distribute.org/distribute_setup.py
+#sudo python distribute_setup.py
+#sudo rm distribute*.gz
+#rm distribute_setup.py
 
-# geos - this gives 3.3.3, we need 3.3.4, hence the manual install above
-#sudo apt-add-repository ppa:sharpie/for-science
-#sudo apt-get -y -q update
-#sudo apt-get -y -q install libgeos-dev
-
-# proj4
-wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
-tar -xvf proj-4.8.0.tar.gz
-cd proj-4.8.0
-./configure
-make
-sudo make install
-cd ..
-rm proj-4.8.0.tar.gz
-rm -rf proj-4.8.0
+## proj4
+#wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
+#tar -xvf proj-4.8.0.tar.gz
+#cd proj-4.8.0
+#./configure
+#make
+#sudo make install
+#cd ..
+#rm proj-4.8.0.tar.gz
+#rm -rf proj-4.8.0
 
 # cartopy
 git clone git://github.com/SciTools/cartopy.git
